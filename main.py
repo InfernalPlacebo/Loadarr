f8f2b7a2a65fe185de7193e3222b109eb5b1cb75
InfernalPlacebo <ip@wfco.xyz>
2024-08-02 20:01:42 -0600

    Created main to provide option to run in autonomous or interactive modes

---
+++ main.py
@@ -0,0 +1,28 @@
+import argparse
+from loadarr import generate_interactive, generate_autonomous
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Loadarr Docker Compose Generator")
+    parser.add_argument('--default', help="Use default settings", action="store")
+    parser.add_argument('--mode', choices=['interactive', 'autonomous'], default='autonomous', help="Choose the mode"
+                                                                                                    " to run the script")
+    parser.add_argument('--media_server', help="Media server choice for autonomous mode")
+    parser.add_argument('--torrent_client', help="Torrent client choice for autonomous mode")
+    parser.add_argument('--usenet_client', help="Usenet client choice for autonomous mode")
+    parser.add_argument('--downloader', help="Downloader choice for autonomous mode")
+    parser.add_argument('--extra_services', help="Extra services choice for autonomous mode")
+
+    args = parser.parse_args()
+
+    if args.mode == 'interactive':
+        generate_interactive()
+    elif args.mode == 'autonomous':
+        if not all([args.media_server, args.torrent_client, args.usenet_client, args.downloader, args.extra_services]):
+            parser.error("All choices must be provided for in autonomous mode")
+        generate_autonomous(args.media_server, args.torrent_client, args.usenet_client, args.downloader,
+                            args.extra_services)
+
+
+if __name__ == "__main__":
+    main()
