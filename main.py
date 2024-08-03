33719e9c2801d3c86e2374e38f4b9926edb6bd34
InfernalPlacebo <ip@wfco.xyz>
2024-08-02 20:03:49 -0600

    Add comments to main.py

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ import argparse
 from loadarr import generate_interactive, generate_autonomous
 
 
+# Main function to parse arguments and call the appropriate function based on the mode
 def main():
+    # Create an argument parser for the script
     parser = argparse.ArgumentParser(description="Loadarr Docker Compose Generator")
+
+    # Add arguments to the parser
     parser.add_argument('--default', help="Use default settings", action="store")
     parser.add_argument('--mode', choices=['interactive', 'autonomous'], default='autonomous', help="Choose the mode"
                                                                                                     " to run the script")
@@ -15,9 +19,11 @@ def main():
 
     args = parser.parse_args()
 
+    # Call the appropriate function based on the mode
     if args.mode == 'interactive':
         generate_interactive()
     elif args.mode == 'autonomous':
+        # Check if all choices are provided for in autonomous mode
         if not all([args.media_server, args.torrent_client, args.usenet_client, args.downloader, args.extra_services]):
             parser.error("All choices must be provided for in autonomous mode")
         generate_autonomous(args.media_server, args.torrent_client, args.usenet_client, args.downloader,
